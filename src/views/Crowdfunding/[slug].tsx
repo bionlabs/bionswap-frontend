cc1bfb3de5f6cee8676220049f9e43dc779af01b
Claris <claris@jadelabs.org>
2022-07-14 16:30:00 +0700

    responsive crowdfunding

--- src/views/Crowdfunding/[slug].tsx
+++ src/views/Crowdfunding/[slug].tsx
@@ -1,9 +1,3 @@
-import Head from "next/head";
-import Menu from 'views/Menu'
-import Footer from 'views/Footer'
-import TitleTag from 'components/TitleTag'
-import PrimaryButton from 'components/PrimaryButton'
-import ProjectItem from './components/ProjectItem'
 import { crowdfundingConfig } from "./config";
 import {
     Box,
@@ -12,18 +6,24 @@ import {
 import Breadcrumb from './components/Breadcrumb'
 import HeadDetail from './components/HeadDetail'
 import FundraiseArea from './components/FundraiseArea'
+import RecomendProjects from "./components/RecomendProjects";
+import TabsArea from "./components/TabsArea";
+import styled from '@emotion/styled'
 
 const CrowdfundingDetail = () => {
+    const Section = styled(Box)`
+        margin-top: 120px;
+    `
     return (
-        <div>
-            <Box component='section'>
-                <Container>
-                    <Breadcrumb name={crowdfundingConfig[0].name} />
-                    <HeadDetail avarta={crowdfundingConfig[0].coin.image} name={crowdfundingConfig[0].name} type={crowdfundingConfig[0].type} unit={crowdfundingConfig[0].fundraiseGoal.unit} />
-                    <FundraiseArea data={crowdfundingConfig[0]} />
-                </Container>
-            </Box>
-        </div >
+        <Section component='section'>
+            <Container>
+                <Breadcrumb name={crowdfundingConfig[0].name} />
+                <HeadDetail avarta={crowdfundingConfig[0].coin.image} name={crowdfundingConfig[0].name} type={crowdfundingConfig[0].type} unit={crowdfundingConfig[0].fundraiseGoal.unit} />
+                <FundraiseArea data={crowdfundingConfig[0]} />
+                <TabsArea data={crowdfundingConfig[0]} />
+                <RecomendProjects data={crowdfundingConfig} />
+            </Container>
+        </Section>
     );
 };
 
