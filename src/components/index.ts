6e9aae366f2f373d270a153e5bc9cb5fc591ac1e
Claris <claris@jadelabs.org>
2022-07-18 11:55:06 +0700

    fix error HHTML when connect wallet

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,8 @@
-export { default as ConnectButton } from "./ConnectButton";
-export { default as WalletInfo } from "./WalletInfo";
+import dynamic from "next/dynamic";
+
+export const ConnectButton = dynamic(() => import("./ConnectButton"), {
+    ssr: false,
+  });
+  export const WalletInfo = dynamic(() => import("./WalletInfo"), {
+    ssr: false,
+  });
