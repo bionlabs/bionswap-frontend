025548583396a6822c3af4bfaf8ba89e403c443b
Pham Quang Huy <quanhuyc12ltqtm@gmail.com>
2022-10-01 23:57:32 +0700

    working avatar

--- src/components/AvatarModal/index.tsx
+++ src/components/AvatarModal/index.tsx
@@ -13,19 +13,9 @@ import {
     Typography,
   } from '@mui/material';
   import { BaseModal } from 'components';
-  import { useCallback, useState } from 'react';
   import CloseIcon from '@mui/icons-material/Close';
-  import { useStandardTokenContractFactory } from 'hooks/useContract';
-  import { parseEther } from 'ethers/lib/utils';
   
-  const tokenTypes = [
-    {
-      value: 0,
-      title: 'Standard Token',
-    },
-  ];
-  
-  const AvatarModal = ({ open, onDismiss, avatars }: any) => {
+  const AvatarModal = ({ open, onDismiss, avatars, handleChooseAvatar }: any) => {
     return (
       <BaseModal
         open={open}
@@ -33,7 +23,6 @@ import {
           padding: '24px',
           maxWidth: '556px',
           width: '100%',
-          height: '90vh',
           overflowY: 'auto',
         }}
       >
@@ -43,7 +32,7 @@ import {
         <FlexBox gap='30px' flexWrap='wrap'>
             {
                 avatars?.map((item: any) => (
-                    <Avarta key={item.title}>
+                    <Avarta key={item.title} onClick={() => handleChooseAvatar(item.avatar)}>
                         <img src={item.avatar} alt={item.title} />
                     </Avarta>
                 ))
@@ -62,70 +51,12 @@ import {
     border-radius: 50%;
     border: 2px solid rgb(12, 22, 32);
     overflow: hidden;
+    cursor: pointer;
 
     img {
         width: 100%;
         height: 100%;
         object-fit: cover;
-    }
-  `;
-  const InputCustom = styled(OutlinedInput)`
-    fieldset {
-      display: none;
-    }
-  
-    input {
-      font-family: 'Poppins', sans-serif;
-      padding: 12px 16px;
-      border: 1px solid;
-      border-color: ${(props) => props.theme.palette.gray[700]};
-      border-radius: 4px;
-      font-weight: 400;
-      font-size: 14px;
-      line-height: 180%;
-      color: ${(props) => props.theme.palette.text.primary};
-  
-      &::placeholder {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 180%;
-        color: ${(props) => props.theme.palette.gray[700]};
-        opacity: 1;
-      }
-    }
-  
-    &.Mui-focused {
-      input {
-        border-color: #9a6aff;
-        box-shadow: rgba(175, 137, 255, 0.4) 0px 0px 0px 2px, rgba(175, 137, 255, 0.65) 0px 4px 6px -1px,
-          rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
-      }
-    }
-  
-    &.onError {
-      input {
-        border-color: ${(props) => props.theme.palette.red[500]};
-        box-shadow: none;
-      }
-    }
-  `;
-  const CheckboxCustom = styled(Checkbox)`
-    color: ${(props) => props.theme.palette.gray[400]};
-  `;
-  const CreateToken = styled(Button)`
-    width: 100%;
-    height: 52px;
-    align-item: center;
-    justify-content: center;
-    display: flex;
-    background-color: ${(props) => props.theme.palette.primary.main};
-    border-radius: 4px;
-  
-    &.Mui-disabled {
-      color: rgba(255, 255, 255, 0.3);
-      box-shadow: none;
-      background-color: rgba(255, 255, 255, 0.12);
     }
   `;
   
--- src/views/Dashboard/views/Overview/components/Header.tsx
+++ src/views/Dashboard/views/Overview/components/Header.tsx
@@ -1,98 +1,97 @@
-import React, { useState } from 'react'
-import {
-    styled,
-    Box,
-    Typography,
-} from '@mui/material'
+import React, { useState } from 'react';
+import { styled, Box, Typography } from '@mui/material';
 import VerifiedIcon from '@mui/icons-material/Verified';
-import { useAccount } from 'wagmi'
-import { shortenAddress } from "utils/format";
+import { useAccount } from 'wagmi';
+import { shortenAddress } from 'utils/format';
 import Image from 'next/image';
 import { MobileProp } from 'configs/Type/Mobile/type';
 import AvatarModal from 'components/AvatarModal';
 import { avatarConfig } from '../config';
 
-const Header = ({isMobile}:MobileProp) => {
+const Header = ({ isMobile }: MobileProp) => {
   const { address } = useAccount();
-  const [openModal, setOpenModal] = useState(false)
-  const [hoverAvatar , setHoverAvatar] = useState(false)
-
-  console.log(hoverAvatar)
+  const [openModal, setOpenModal] = useState(false);
+  const [hoverAvatar, setHoverAvatar] = useState(false);
+  const [avatar, setAvatar] = useState('');
 
   const handleToggleModal = () => {
     setOpenModal(!openModal);
-  }
+  };
+
+  const handleChooseAvatar = (param: string) => {
+    setAvatar(param);
+    setOpenModal(false);
+  };
 
   return (
     <Box>
-        <Box display='flex' alignItems={isMobile ? 'start' : 'center'} gap='15px' flexDirection={isMobile ? 'column' : 'row'}>
-            <AvatarBox
-                onMouseEnter={() => setHoverAvatar(true)}
-                onMouseLeave={() => setHoverAvatar(false)}
-                onClick={handleToggleModal}
-            >
-                <Image src='/icons/dashboard/user.svg' alt='' width='120px' height='120px' />
-                <Box sx={{
-                    position: 'absolute', top: '40%', left: '13%',
-                    display: hoverAvatar ? 'block' : 'none'
-                }}>
-                    <Typography variant='captionPoppins' sx={{color: 'primary.main'}}>
-                        Change avatar
-                    </Typography>
-                </Box>
-            </AvatarBox>
+      <Box
+        display="flex"
+        alignItems={isMobile ? 'start' : 'center'}
+        gap="15px"
+        flexDirection={isMobile ? 'column' : 'row'}
+      >
+        <AvatarBox
+          onMouseEnter={() => setHoverAvatar(true)}
+          onMouseLeave={() => setHoverAvatar(false)}
+          onClick={handleToggleModal}
+        >
+          <Image src={avatar ? avatar : '/icons/dashboard/user.svg'} alt="" width="120px" height="120px" />
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '40%',
+              left: '13%',
+              display: hoverAvatar ? 'block' : 'none',
+            }}
+          >
+            <Typography variant="captionPoppins" sx={{ color: 'primary.main' }}>
+              Change avatar
+            </Typography>
+          </Box>
+        </AvatarBox>
+        <Box>
+          <Box display="flex" alignItems="center" gap="10px">
+            <Typography variant="h6Samsung">{address ? 'Anonymous-User' : 'N/A'}</Typography>
             <Box>
-                <Box display='flex' alignItems='center' gap='10px'>
-                    <Typography variant='h6Samsung'>
-                        {address ? 'Anonymous-User' : 'N/A'}
-                    </Typography>
-                    <Box>
-                        <Tag sx={{backgroundColor: 'gray.700'}}>
-                            Personal
-                        </Tag>
-                    </Box>
-                </Box>
-                <Flex mt='10px' gap={isMobile ? '15px' : '40px'}>
-                    <Box>
-                        <Box>
-                            <Typography variant='captionPoppins' sx={{color: 'gray.400'}}>
-                                Wallet Address
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Typography sx={{fontSize: '15px'}}>
-                                {address ? shortenAddress(address) : 'N/A'}
-                            </Typography>
-                        </Box>
-                    </Box>
-                    <Box>
-                        <Box>
-                            <Typography variant='captionPoppins' sx={{color: 'gray.400'}}>
-                            User Type
-                            </Typography>
-                        </Box>
-                        <Box display='flex' alignItems='center' gap='10px'>
-                            <Typography sx={{fontSize: '15px' }}>
-                                {address ? 'Regular User' : 'N/A'}
-                            </Typography>
-                            {/* <Box>
+              <Tag sx={{ backgroundColor: 'gray.700' }}>Personal</Tag>
+            </Box>
+          </Box>
+          <Flex mt="10px" gap={isMobile ? '15px' : '40px'}>
+            <Box>
+              <Box>
+                <Typography variant="captionPoppins" sx={{ color: 'gray.400' }}>
+                  Wallet Address
+                </Typography>
+              </Box>
+              <Box>
+                <Typography sx={{ fontSize: '15px' }}>{address ? shortenAddress(address) : 'N/A'}</Typography>
+              </Box>
+            </Box>
+            <Box>
+              <Box>
+                <Typography variant="captionPoppins" sx={{ color: 'gray.400' }}>
+                  User Type
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center" gap="10px">
+                <Typography sx={{ fontSize: '15px' }}>{address ? 'Regular User' : 'N/A'}</Typography>
+                {/* <Box>
                                 <VerifiedIcon sx={{width: '18px',height:'18px', color: 'primary.main'}} />
                             </Box> */}
-                        </Box>
-                    </Box>
-                    <Box>
-                        <Box>
-                            <Typography variant='captionPoppins' sx={{color: 'gray.400'}}>
-                            Twitter
-                            </Typography>
-                        </Box>
-                        <Box display='flex' alignItems='center' gap='10px'>
-                            <Typography sx={{fontSize: '15px'}}>
-                                {address ? 'not linked' : 'N/A'}
-                            </Typography>
-                        </Box>
-                    </Box>
-                    {/* <Box>
+              </Box>
+            </Box>
+            <Box>
+              <Box>
+                <Typography variant="captionPoppins" sx={{ color: 'gray.400' }}>
+                  Twitter
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center" gap="10px">
+                <Typography sx={{ fontSize: '15px' }}>{address ? 'not linked' : 'N/A'}</Typography>
+              </Box>
+            </Box>
+            {/* <Box>
                         <Box>
                             <Typography variant='captionPoppins' sx={{color: 'gray.400'}}>
                             Last Connect
@@ -104,39 +103,48 @@ const Header = ({isMobile}:MobileProp) => {
                             </Typography>
                         </Box>
                     </Box> */}
-                </Flex>
-            </Box>
+          </Flex>
         </Box>
-        <AvatarModal open={openModal} onDismiss={handleToggleModal} avatars={avatarConfig} />
+      </Box>
+      <AvatarModal
+        open={openModal}
+        onDismiss={handleToggleModal}
+        avatars={avatarConfig}
+        handleChooseAvatar={handleChooseAvatar}
+      />
     </Box>
-  )
-}
+  );
+};
 
 const Flex = styled(Box)`
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`;
 const Tag = styled(Box)`
-    border-radius: 4px;
-    color: #fff;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 5px 8px;
-    font-size: 12px;
-`
+  border-radius: 4px;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 8px;
+  font-size: 12px;
+`;
 const AvatarBox = styled(Box)`
-    * {
-        transition: .12s ease-in;
+  * {
+    transition: 0.12s ease-in;
+  }
+  position: relative;
+  cursor: pointer;
+  :hover {
+    img {
+      opacity: 0.3;
     }
-    position: relative;
-    cursor: pointer;
-    :hover {
-        img {
-            opacity: .3
-        }
-    }
-`
+  }
 
-export default Header
+  img {
+    border-radius: 50%;
+  }
+`;
+
+export default Header;
