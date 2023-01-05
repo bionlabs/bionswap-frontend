1ecd367120d9326c49b993564eeacf7d9772b58b
Duy Nguyen <duyngph@gmail.com>
2023-01-05 16:00:45 +0700

    add myProject page

---
+++ src/pages/dashboard/my-project/[slug].tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from 'components/Sidebar';
+import MyProjectDetail from 'views/MyProjectDetail';
+import tabsConfig from 'views/Dashboard/tabsConfig';
+
+const AssetRef = () => {
+  
+  return (
+    <MyProjectDetail />
+  )
+}
+
+export default AssetRef
---
+++ src/pages/dashboard/my-project/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Allocation from "views/Dashboard/views/Allocation/Allocation";
+import { useRouter } from 'next/router';
+import Sidebar from 'components/Sidebar';
+import MyProject from 'views/MyProject';
+import tabsConfig from 'views/Dashboard/tabsConfig';
+
+const AssetRef = () => {
+
+  return (
+    <MyProject />
+  )
+}
+
+export default AssetRef
