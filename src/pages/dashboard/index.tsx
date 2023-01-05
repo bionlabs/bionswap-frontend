a824f251760e22eb80cb9ab6de9a6d718f2e9d48
Tú Hình <htinhtu@gmail.com>
2023-01-06 02:07:28 +0700

    feat: add nested layout - Related to https://bionswap.atlassian.net/browse/BIONSWAP-1

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,14 @@
-import Dashboard from "views/Dashboard"
+import { NextPageWithLayout } from 'pages/_app';
+import { ReactElement } from 'react';
+import Dashboard from 'views/Dashboard';
+import DashboardLayout from 'views/Dashboard/layout';
 
-export default Dashboard
+const DashboardPage: NextPageWithLayout = () => {
+  return <Dashboard />;
+};
+
+DashboardPage.getLayout = function getLayout(page: ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
+
+export default DashboardPage;
--- src/views/Dashboard/tabsConfig.tsx
+++ src/views/Dashboard/tabsConfig.tsx
@@ -1,30 +1,30 @@
-import AllocationIcon from "assets/icons/AllocationIcon"
-import LaunchpadIcon from "assets/icons/LaunchpadIcon"
-import OverviewIcon from "assets/icons/OverviewIcon"
-import MyProject from "views/MyProject"
-import Allocation from "views/Dashboard/views/Allocation/Allocation"
-import Overview from "views/Dashboard/views/Overview/Overview"
-import MyProjectsIcon from "assets/icons/MyProjectsIcon"
+import AllocationIcon from 'assets/icons/AllocationIcon';
+import LaunchpadIcon from 'assets/icons/LaunchpadIcon';
+import OverviewIcon from 'assets/icons/OverviewIcon';
+import MyProject from 'views/MyProject';
+import Allocation from 'views/Dashboard/views/Allocation/Allocation';
+import Overview from 'views/Dashboard/views/Overview/Overview';
+import MyProjectsIcon from 'assets/icons/MyProjectsIcon';
 
 const tabsConfig = [
-    {
-        label: 'Overview',
-        href: '/dashboard/overview',
-        icon: OverviewIcon,
-        component: <Overview/>
-    },
-    {
-        label: 'Allocation',
-        href: '/dashboard/allocation',
-        icon: AllocationIcon,
-        component: <Allocation/>
-    },
-    {
-        label: 'My Projects',
-        href: '/dashboard/my-project',
-        icon: MyProjectsIcon,
-        component: <MyProject/>
-    },
-]
+  {
+    label: 'Overview',
+    href: '/dashboard',
+    icon: OverviewIcon,
+    component: <Overview />,
+  },
+  {
+    label: 'Allocation',
+    href: '/dashboard/allocation',
+    icon: AllocationIcon,
+    component: <Allocation />,
+  },
+  {
+    label: 'My Projects',
+    href: '/dashboard/my-project',
+    icon: MyProjectsIcon,
+    component: <MyProject />,
+  },
+];
 
-export default tabsConfig
+export default tabsConfig;
