3a6b3ff43af68c0e18ada87c1cf5f52e8046ef2e
Tú Hình <htinhtu@gmail.com>
2023-02-24 17:50:39 +0700

    add select chain

--- src/pages/earn/add/index.tsx
+++ src/pages/earn/add/index.tsx
@@ -1,12 +1,40 @@
-import { Box, Button, Container, FormControlLabel, Select, styled, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  FormControlLabel,
+  Select,
+  styled,
+  Stack,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '@mui/material';
 import { useState } from 'react';
 import Page from 'components/Page';
 import { useRouter } from 'next/router';
 import HomeIcon from '@mui/icons-material/Home';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { CHAIN_INFO_MAP } from 'configs/chain';
+import Image from 'next/image';
+import { getChainIcon } from 'utils/chains';
+import { Chain } from 'wagmi';
 
-const AddPool = ({ chainId }: any) => {
+const AddPool = () => {
   const [view, setView] = useState<string | null>('card');
+  const [chain, setChain] = useState<Chain>({
+    id: 97,
+    name: 'BNB Testnet',
+    network: 'bsc-testnet',
+    testnet: true,
+    nativeCurrency: { name: 'BNB', symbol: 'BNB', decimals: 18 },
+    rpcUrls: { default: 'https://rpc.ankr.com/bsc_testnet_chapel' },
+    blockExplorers: {
+      etherscan: { name: 'BNB Chain Explorer', url: 'https://testnet.bscscan.com' },
+      default: { name: 'BNB Chain Explorer', url: 'https://testnet.bscscan.com' },
+    },
+  });
   const router = useRouter();
   const [iconColor, setIconColor] = useState('#475569');
   const handleChangeView = (event: React.MouseEvent<HTMLElement>, newView: string | null) => {
@@ -50,6 +78,71 @@ const AddPool = ({ chainId }: any) => {
               <Typography fontSize="14px" fontWeight="500">
                 Add
               </Typography>
+            </Box>
+            <Box>
+              <Accordion
+                sx={{
+                  backgroundColor: (theme) => (theme.palette as any).extra.swapPanel.panel,
+                  backgroundImage: 'none',
+                  borderRadius: '8px!important',
+                  boxShadow: 'none',
+                  width: '100%',
+                }}
+              >
+                <AccordionSummary
+                  expandIcon={
+                    <Image
+                      src={getChainIcon(chain?.id ?? 97)?.iconUrl}
+                      layout="fixed"
+                      alt=""
+                      width={15}
+                      height={15}
+                      style={{ marginLeft: '15px' }}
+                    />
+                  }
+                  sx={{
+                    '& .MuiAccordionSummary-expandIconWrapper': {
+                      transition: 'none',
+                      '&.Mui-expanded': {
+                        transform: 'none',
+                      },
+                    },
+                  }}
+                >
+                  1. Select Network
+                </AccordionSummary>
+
+                <AccordionDetails>
+                  <div>
+                    Selected:{' '}
+                    <Typography fontSize="14px" fontWeight={600}>
+                      {chain?.name}
+                    </Typography>
+                  </div>
+                  <Stack direction="row" spacing={1} sx={{ marginTop: '10px' }}>
+                    {Object.entries(CHAIN_INFO_MAP).map(([, chainData]) => (
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          borderRadius: '30%',
+                          width: '40px',
+                          height: '40px',
+                          backgroundColor: chain.id === chainData.id ? '#313F52' : '',
+                          ':hover': {
+                            border: '1px solid #313F52',
+                          },
+                          cursor: 'pointer',
+                        }}
+                        onClick={() => setChain(chainData)}
+                      >
+                        <Image src={getChainIcon(chainData.id)?.iconUrl} layout="fixed" alt="" width={15} height={15} />
+                      </Box>
+                    ))}
+                  </Stack>
+                </AccordionDetails>
+              </Accordion>
             </Box>
           </Stack>
         </Wrapper>
